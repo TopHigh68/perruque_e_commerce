b4c6a6f9f2cec035d392d68fd170560e82bdb881
TopHigh68 <inocenthighfive60@gmail.com>
2026-01-31 13:42:19 +0100

    fix

---
+++ frontend/src/assets/images.ts
@@ -0,0 +1,54 @@
+// Import all wig images
+import perruque1 from '@/assets/perruques/perruque1.jpg';
+import perruque2 from '@/assets/perruques/perruque2.jpg';
+import perruque3 from '@/assets/perruques/perruque3.jpg';
+import perruque4 from '@/assets/perruques/perruque4.jpg';
+import perruque5 from '@/assets/perruques/perruque5.jpg';
+import perruque6 from '@/assets/perruques/perruque6.jpg';
+import perruque7 from '@/assets/perruques/perruque7.jpg';
+import perruque8 from '@/assets/perruques/perruque8.jpg';
+import perruque9 from '@/assets/perruques/perruque9.jpg';
+import perruque10 from '@/assets/perruques/perruque10.jpg';
+import perruque11 from '@/assets/perruques/perruque11.jpg';
+import perruque12 from '@/assets/perruques/perruque12.jpg';
+import perruque13 from '@/assets/perruques/perruque13.jpg';
+import perruque14 from '@/assets/perruques/perruque14.jpg';
+import perruque15 from '@/assets/perruques/perruque15.jpg';
+import perruque16 from '@/assets/perruques/perruque16.jpg';
+import perruque17 from '@/assets/perruques/perruque17.jpg';
+import perruque18 from '@/assets/perruques/perruque18.jpg';
+import perruque19 from '@/assets/perruques/perruque19.jpg';
+import perruque20 from '@/assets/perruques/perruque20.jpg';
+import wigProduct1 from '@/assets/wig-product-1.jpg';
+import wigProduct2 from '@/assets/wig-product-2.jpg';
+import wigProduct3 from '@/assets/wig-product-3.jpg';
+import wigProduct4 from '@/assets/wig-product-4.jpg';
+import logo from '@/assets/logo/logo.png';
+
+export {
+  logo,
+  perruque1,
+  perruque2,
+  perruque3,
+  perruque4,
+  perruque5,
+  perruque6,
+  perruque7,
+  perruque8,
+  perruque9,
+  perruque10,
+  perruque11,
+  perruque12,
+  perruque13,
+  perruque14,
+  perruque15,
+  perruque16,
+  perruque17,
+  perruque18,
+  perruque19,
+  perruque20,
+  wigProduct1,
+  wigProduct2,
+  wigProduct3,
+  wigProduct4
+};
